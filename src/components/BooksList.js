acd7357413deb731d8051934fe54e03081d93196
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-09-30 12:47:34 +0100

    refactor:Call to the server and fetch data

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
 
 class BooksList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+  componentWillMount() {
+    // call to the server
+    const url = "https://abolkog.com/api/books";
+
+    axios.get(url).then(response => {
+      const books = response.data.books;
+      this.setState({ books });
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>Books List </Text>
+        {this.state.books.map(book => {
+          return (
+            <View>
+              <Text>{book.name}</Text>
+              <Text>{book.price}</Text>
+            </View>
+          );
+        })}
       </View>
     );
   }
