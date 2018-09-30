2457b8e32754fa54fdf84f6d4f62ece5d68757ac
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-09-30 15:44:56 +0100

    feat:Create Button statelessComoponent

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const Button = props => {
+  return (
+    <TouchableOpacity style={styles.buttonStyle}
+      onPress={() => console.log('buuton clicked')}
+     >
+      <Text style={styles.textStyle}>Buy Now</Text>
+    </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+  buttonStyle: {
+      height:35,
+      flex: 1,
+      borderRadius: 5,
+      justifyContent: 'center',
+      backgroundColor: '#2a3744',
+      marginVertical: 20
+  },
+  textStyle: {
+   color:'#82b541',
+   textAlign:'center',
+   fontSize:15,
+   fontWeight: 'bold',
+  }
+});
+
+export default Button;
